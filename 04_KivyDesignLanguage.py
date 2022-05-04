62202706e1a98b0ae8521433f405745f11ffb531
ManideepBangaru <bmd994@gmail.com>
2022-05-05 00:27:00 +0530

    accessed variables from .kv file

--- 04_KivyDesignLanguage.py
+++ 04_KivyDesignLanguage.py
@@ -1,22 +1,25 @@
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.widget import Widget
-
+from kivy.properties import ObjectProperty
 
 class MyGridLayout(Widget):
-    def press(self, instance):
-        name = self.top_grid.name.text
-        pizza = self.top_grid.pizza.text
-        color = self.top_grid.Color.text
+    name = ObjectProperty(None)
+    pizza = ObjectProperty(None)
+    color = ObjectProperty(None)
+
+    def press(self):
+        name = self.name.text
+        pizza = self.pizza.text
+        color = self.color.text
         # print to the terminal
-        # print("Hello %s, you like %s and your favourite color is %s"%(name,pizza,color))
+        print("Hello %s, you like %s and your favourite color is %s"%(name,pizza,color))
         # print to the screen
         # self.add_widget(Label(text="Hello %s, you like %s and your favourite color is %s" % (name, pizza, color),
         #                       font_size = 20))
         # # clear the input boxes
-        # self.top_grid.name.text = ""
-        # self.top_grid.pizza.text = ""
-        # self.top_grid.Color.text = ""
+        self.name.text = ""
+        self.pizza.text = ""
+        self.color.text = ""
 
 
 class MyApp(App):
