dbbbc33b02cba40d56902f7aeafd3de948c529df
ManideepBangaru <bmd994@gmail.com>
2022-05-05 00:37:18 +0530

    trying out color button

---
+++ 06_ButtonColors.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+
+# Builder.load_file("my.kv")
+
+class MyGridLayout(Widget):
+    name = ObjectProperty(None)
+    pizza = ObjectProperty(None)
+    color = ObjectProperty(None)
+
+    def press(self):
+        name = self.name.text
+        pizza = self.pizza.text
+        color = self.color.text
+        # print to the terminal
+        print("Hello %s, you like %s and your favourite color is %s"%(name,pizza,color))
+        # print to the screen
+        # self.add_widget(Label(text="Hello %s, you like %s and your favourite color is %s" % (name, pizza, color),
+        #                       font_size = 20))
+        # # clear the input boxes
+        self.name.text = ""
+        self.pizza.text = ""
+        self.color.text = ""
+
+
+class MyApp(App):
+    def build(self):
+        return MyGridLayout()
+
+if __name__ == "__main__":
+    MyApp().run()
