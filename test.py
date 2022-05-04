dbfd32c4656175760b87458412073297fdab466b
ManideepBangaru <bmd994@gmail.com>
2022-05-04 23:01:23 +0530

    practiced the app

--- test.py
+++ test.py
@@ -1,37 +1,49 @@
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-
 
 class MyGridLayout(GridLayout):
     def __init__(self,**kwargs):
         super(MyGridLayout, self).__init__(**kwargs)
-        self.cols = 2
 
-        self.add_widget(Label(text = "Name",font_size=20))
-        self.name = TextInput(multiline=False,font_size=20)
-        self.add_widget(self.name)
+        self.cols = 1
 
-        self.add_widget(Label(text="Favourite Pizza",font_size=20))
-        self.pizza = TextInput(multiline=False,font_size=20)
-        self.add_widget(self.pizza)
+        self.top_grid = GridLayout()
+        self.top_grid.cols = 2
 
-        self.add_widget(Label(text="Favourite Color",font_size=20))
-        self.color = TextInput(multiline=False,font_size=20)
-        self.add_widget(self.color)
+        self.top_grid.add_widget(Label(text = "Name : ", font_size = 20))
 
-        self.submit = Button(text="submit",font_size=20)
+        self.top_grid.name = TextInput(multiline=False, font_size = 20)
+        self.top_grid.add_widget(self.top_grid.name)
+
+        self.top_grid.add_widget(Label(text="Your Favourite Pizza : ",font_size=20))
+        self.top_grid.pizza = TextInput(multiline=False, font_size = 20)
+        self.top_grid.add_widget(self.top_grid.pizza)
+
+        self.add_widget(self.top_grid)
+
+        self.submit = Button(text="submit", font_size=15,
+                             size_hint_y=None,
+                             height = 50)
+        self.submit.bind(on_press=self.press)
         self.add_widget(self.submit)
 
+    def press(self,instance):
+        name = self.top_grid.name.text
+        pizza = self.top_grid.pizza.text
+        self.add_widget(Label(text="Hi %s, you like %s pizza"%(name,pizza)))
+        self.top_grid.name.text = ""
+        self.top_grid.pizza.text = ""
 
 
 
 
 class MyApp(App):
     def build(self):
+        # return Label(text = "Hello")
         return MyGridLayout()
 
-if __name__ == "__main__":
+if __name__=="__main__":
     MyApp().run()
